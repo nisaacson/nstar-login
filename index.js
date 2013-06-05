32c402cdd795ba38ac0f7901f8d33671bf124838
Noah Isaacson <noah@nisaacson.com>
2013-06-05 10:55:52 -0600

    first commit

---
+++ index.js
@@ -0,0 +1,9 @@
+/*
+ * @param (data) login record object containing username, password,
+ * and answers to secret questions
+*/
+var rk = require('required-keys')
+module.exports = function login (data, cb) {
+   // login to nstar website here
+
+}
